6800e5d16c8141c66be30051027daf7d21a7e9bb
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-08-19 12:30:12 +0300

    chagge urlencoded

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getPaymentInfo, sendHtmlForm, sendParams } = require('../services/liqpayCallback');
 
-router.post('/liqpay-payment-info', getPaymentInfo);
+router.post('/callback', getPaymentInfo);
 
 router.post('/pay', sendHtmlForm)
 router.post('/pay-button', sendParams)
--- services/liqpayCallback.js
+++ services/liqpayCallback.js
@@ -8,44 +8,32 @@ const liqpay = new LiqPay(public_key, private_key);
 
 const getPaymentInfo = async (req, res, next) => {
   const { data, signature } = req.body;
+  console.log('req.body', req.body);
   console.log('=========> message from bank', `${data}, ===>>>> ${signature}`);
-  //   try {
-  //     res.send({
-  //       status: 'Upload was successful',
-  //       result: req.body,
-  //       ressulTwo: {
-  //         data,
-  //         signature,
-  //       },
-  //     });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // };
-  // async ({ data, signature }) => {
-  const compSignature = liqpay.str_to_sign(private_key + data + private_key);
-  console.log('compSignature ', compSignature);
 
-  if (signature === compSignature) {
-    const buff = Buffer.from(data, 'base64');
-    let text = buff.toString('utf-8');
-    text = JSON.parse(text);
-    console.log('text ', text);
+  // const compSignature = liqpay.str_to_sign(private_key + data + private_key);
+  // console.log('compSignature ', compSignature);
 
-    const { status, amount, order_id, payment_id } = text;
+  // if (signature === compSignature) {
+  //   const buff = Buffer.from(data, 'base64');
+  //   let text = buff.toString('utf-8');
+  //   text = JSON.parse(text);
+  //   console.log('text ', text);
 
-    return res.send({
-      status: 'Payment check',
-      resultFrom: {
-        data,
-        signature,
-      },
-      text: { status, amount, order_id, payment_id },
-    });
-  }
+  //   const { status, amount, order_id, payment_id } = text;
+
+  //   return res.send({
+  //     status: 'Payment check',
+  //     resultFrom: {
+  //       data,
+  //       signature,
+  //     },
+  //     text: { status, amount, order_id, payment_id },
+  //   });
+  // }
 
   res.send({
-    status: 'signature !== compSignature',
+    // status: 'signature !== compSignature',
     result: req.body,
   });
 };
@@ -62,7 +50,7 @@ const sendHtmlForm = async (req, res, next) => {
     order_id: uuidV4(),
     version: '3',
     language: 'uk',
-    server_url: 'https://payment-server-node.onrender.com/liqpay-payment-info',
+    server_url: 'https://payment-server-node.onrender.com/callback',
     result_url: 'https://smisyuk4.github.io/payment-services',
   });
 
@@ -81,7 +69,7 @@ const sendParams = async (req, res, next) => {
     order_id: uuidV4(),
     version: '3',
     language: 'uk',
-    server_url: 'https://payment-server-node.onrender.com/liqpay-payment-info',
+    server_url: 'https://payment-server-node.onrender.com/callback',
     result_url: 'https://smisyuk4.github.io/payment-services',
   };
 
